63c51404b74677bda8079fe2ee070f35445c9859
Eric Marchand <eric.marchand@4d.com>
2022-11-06 15:31:44 +0100

    Fix test by upgrading XcodeProjKit

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/phimage/XcodeProjKit.git", from: "3.0.2")
+        .package(url: "https://github.com/phimage/XcodeProjKit.git", from: "3.0.4")
     ],
     targets: [
         .executableTarget(
